114fa99d534c4c810a1ab787c33b6d58b7fde727
Seher Nur GUL <sehernege@gmail.com>
2021-05-24 10:38:57 +0300

    Update ErrorDataResult.java

--- northwind/northwind/src/main/java/kodlamaio/northwind/core/utilities/results/ErrorDataResult.java
+++ northwind/northwind/src/main/java/kodlamaio/northwind/core/utilities/results/ErrorDataResult.java
@@ -2,23 +2,23 @@ package kodlamaio.northwind.core.utilities.results;
 
 public class ErrorDataResult<T> extends DataResult<T> {
 	public ErrorDataResult(T data, String message) {
-		super(data, true, message);
+		super(data, false, message);
 	}
 
 	//mesaj döndürmek istemediğimizde
 	//error olmasına rağmen data gönderiyoruz
 	public ErrorDataResult(T data) {
-		super(data, true);
+		super(data, false);
 	}
 	
 	//data döndürmek istemediğimizde
 	public ErrorDataResult(String message) {
-		super(null, true, message);
+		super(null, false, message);
 	}
 
 	
 	//data ve mesaj vermeden
 	public ErrorDataResult() {
-		super(null, true);
+		super(null, false);
 	}
 }
